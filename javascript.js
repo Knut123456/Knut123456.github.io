394605b9f352bbf78d0a854cc6b6cbc934b1f004
Knut123456 <knroa002@osloskolen.no>
2024-03-01 09:16:32 +0100

    ga bilde en lenke, gjorde slik at teks og bilder er på rett ved siden av hverandre.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,4 @@
-const link1 = document.getElementById ("link1")
 
-
-
-link1.addEventListener("click", function () {
-    window.location = "https://github.com/Knut123456/Matoopskrift.git"
-})
+function imglink() {
+    window.location = "https://knut123456.github.io/Matoopskrift/";
+}
