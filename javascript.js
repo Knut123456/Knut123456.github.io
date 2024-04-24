f670d49da63e5cb1901c6ea90654f347bd1e3e0e
Knut123456 <knroa002@osloskolen.no>
2024-04-24 14:23:48 +0200

    vet ikke

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
 
+
 function imglink() {
     window.location = "https://knut123456.github.io/Matoopskrift/";
 }
-linkport1();{
-    window.location = "https://knut123456.github.io/Matoopskrift/";
+function linkport1() {
+    window.location.href = "portfolio.html";
+}
+
+function tilbake() {
+    window.location.href = "index.html";
+}
+
+function linkcd1() {
+    window.location.href = "cd1.html";
 }
