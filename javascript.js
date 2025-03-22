469d4b3ae4c71df1eefed8458d2e1a2c9eb8cba9
Knut123456 <149372587+Knut123456@users.noreply.github.com>
2025-03-22 11:36:31 +0100

    vet ikke

--- javascript.js
+++ javascript.js
@@ -1,17 +1,1 @@
 
-
-
-function portfolio() {
-    window.location.href = "portfolio.html";
-}
-
-function main() {
-    window.location.href = "index.html";
-}
-
-function cv() {
-    window.location.href = "cv.html";
-}
-function ommeg() {
-    window.location.href = "ommeg.html";
-}
